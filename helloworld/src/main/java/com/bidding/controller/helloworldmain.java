5543a1ba877c68614bd61918ea4f0bad9c5ba6eb
刘龙飞 <t-longfei.liu@egreatwall.com>
2022-04-20 11:02:53 +0800

    helloworld程序提交--远程仓库---测试

--- helloworld/src/main/java/com/bidding/controller/helloworldmain.java
+++ helloworld/src/main/java/com/bidding/controller/helloworldmain.java
@@ -23,6 +23,7 @@ public class helloworldmain {
     public  String helloWorld(){
         helloworldmain.class.getClass();
         //读取配置信息
+        //
         String root = env.getProperty("com.cxytiandi.name");
         System.out.print(root);
         return root;
